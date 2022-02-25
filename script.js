33690c103b7332562e949b878ca926ebf0eca89c
Dayane Chiozzi <daychiozzi@gmail.com>
2022-02-25 16:14:44 -0300

    Adicionando requisito 3

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("teste");
+const userEmail = document.getElementById("email");
+const userPassword = document.getElementById("password");
+const submitBtn = document.getElementById("submit-btn");
+
+function checkInfo() {
+  if (
+    userEmail.value === "tryber@teste.com" &&
+    userPassword.value === "123456"
+  ) {
+    alert("Olá, Tryber!");
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+}
+
+submitBtn.addEventListener("click", checkInfo);
